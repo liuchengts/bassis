4fc6e4bc2d8420df3419c347c603debdea75c349
刘成 <790809568@qq.com>
2019-07-30 14:54:18 +0800

    说明

--- bassis-bean/src/main/java/com/bassis/bean/annotation/Aop.java
+++ bassis-bean/src/main/java/com/bassis/bean/annotation/Aop.java
@@ -21,7 +21,7 @@ public @interface Aop {
 
     /**
      * 要执行aop时除了方法本身参数外，还需要的额外参数
-     * 参数组成顺序是，当前parameters数组参数后面加上当前带有@Aop方法的所有入参，按照顺序排列
+     * 参数组成顺序是，当前parameters数组参数后面加上当前带有@Aop方法的所有入参，按照顺序排列,参数为null是用字符串"null"表示
      * 举例  parameters = {"a", "b", "c"} 即实际的aop方法最前面三个参数为 a  b  c  之后才是@Aop方法本身的入参
      */
     String[] parameters() default "";
